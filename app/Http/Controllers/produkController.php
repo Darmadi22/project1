a80b3e3a9b27f144d7c6a5e8cd9399ba22809f3e
Darmadi22 <agusdarmadi2000@gmail.com>
2020-10-12 12:32:38 +0700

    first commit

---
+++ app/Http/Controllers/produkController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class produkController extends Controller
+{
+    //properti indeks
+    public function index(){
+        $isi = "Percobaan";
+        return $isi;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\produkController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function() {
+    echo "ini merupakan page (page about)";
+})->name('tentang');
+
+Route::get('/show/{id?}', function($id) {
+    echo "Id : ". $id ;
+})->where('id','[0-9]+');
+
+Route::get('/utama', function() {
+    echo "Page Utama";
+    echo "<br>";
+    echo "<a href='".route('tentang')."'>ABOUT</a>";
+});
+
+Route::get('/produk',[produkController::class,'index']);
